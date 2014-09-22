f53f45f189f259603fea1a57fbd3d0bab721ded7
Mach5x <nr.chiadmi@gmail.com>
2014-09-22 21:44:17 +0200

    Update Ex3.c

--- Ex3.c
+++ Ex3.c
@@ -1,1 +1,31 @@
-#
+#include <stdlib.>
+#include <stdio.h>
+#include <time.h>
+#include <wait.h>
+
+#define NBMAXESSAIS 3
+
+int main(int argc, char *argv[])
+{
+pid_t idproc;
+int nbAlea, nbEssais=0;
+int codeRetour=0, cr;
+
+do{
+srand(time(null));
+idproc=fork();
+if(idproc==-1)printf(stderr,"Erreur création processus fils.");
+if(idproc==0)
+{
+nbAlea=rand()%100+1;
+execlp("exo3","nbAlea",NULL);
+printf(stderr,"Erreur d'exec");
+exit(0);
+}
+codeRetour=wait(&cr);
+NbEssais++;
+if(WIFEXITED(cr)){
+}
+}while(nbEssais<NBMAXESSAIS || codeRetour!=0);
+
+}
